db722a0582f734bf34a5c0d6180ad6d3a0cc3fd4
Yathish G P <yathishgp1@gmail.com>
2025-07-22 22:06:07 +0530

    add llama4

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
         <div className="mt-10 flex grow flex-col items-center px-4 lg:mt-16">
           <a
             className="mb-4 inline-flex shrink-0 items-center rounded-full border-[0.5px] bg-white px-7 py-2 text-xs text-gray-800 shadow-[0px_1px_1px_0px_rgba(0,0,0,0.25)] md:text-base"
-            href="https://togetherai.link/?utm_source=llamacoder&utm_medium=referral&utm_campaign=example-app"
+            href="https://togetherai.link/?utm_source=example-app&utm_medium=llamacoder&utm_campaign=llamacoder-app-signup"
             target="_blank"
           >
             <span className="text-center">
@@ -342,14 +342,14 @@ export default function Home() {
             <div className="font-medium">
               Built with{" "}
               <a
-                href="https://togetherai.link/?utm_source=llamacoder&utm_medium=referral&utm_campaign=example-app"
+                href="https://togetherai.link/?utm_source=example-app&utm_medium=llamacoder&utm_campaign=llamacoder-app-signup"
                 className="font-semibold text-blue-600 underline-offset-4 transition hover:text-gray-700 hover:underline"
               >
                 Llama
               </a>{" "}
               and{" "}
               <a
-                href="https://togetherai.link/?utm_source=llamacoder&utm_medium=referral&utm_campaign=example-app"
+                href="https://togetherai.link/?utm_source=example-app&utm_medium=llamacoder&utm_campaign=llamacoder-app-signup"
                 className="font-semibold text-blue-600 underline-offset-4 transition hover:text-gray-700 hover:underline"
               >
                 Together AI
@@ -358,7 +358,11 @@ export default function Home() {
             </div>
           </div>
           <div className="flex space-x-4 pb-4 sm:pb-0">
-            <Link href="https://twitter.com" className="group" aria-label="">
+            <Link
+              href="https://twitter.com/nutlope"
+              className="group"
+              aria-label=""
+            >
               <svg
                 aria-hidden="true"
                 className="h-6 w-6 fill-gray-500 group-hover:fill-gray-700"
@@ -366,7 +370,11 @@ export default function Home() {
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0 0 22 5.92a8.19 8.19 0 0 1-2.357.646 4.118 4.118 0 0 0 1.804-2.27 8.224 8.224 0 0 1-2.605.996 4.107 4.107 0 0 0-6.993 3.743 11.65 11.65 0 0 1-8.457-4.287 4.106 4.106 0 0 0 1.27 5.477A4.073 4.073 0 0 1 2.8 9.713v.052a4.105 4.105 0 0 0 3.292 4.022 4.093 4.093 0 0 1-1.853.07 4.108 4.108 0 0 0 3.834 2.85A8.233 8.233 0 0 1 2 18.407a11.615 11.615 0 0 0 6.29 1.84" />
               </svg>
             </Link>
-            <Link href="https://github.com" className="group" aria-label="">
+            <Link
+              href="https://github.com/Nutlope/llamacoder"
+              className="group"
+              aria-label=""
+            >
               <svg
                 aria-hidden="true"
                 className="h-6 w-6 fill-slate-500 group-hover:fill-slate-700"
@@ -405,4 +413,5 @@ function LoadingMessage({
   );
 }
 
+export const runtime = "edge";
 export const maxDuration = 45;
